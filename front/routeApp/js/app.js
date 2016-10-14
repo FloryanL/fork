8484b0ba59496ad3815a2cad4c056a35e68b0192
FloryanL <floryan.lollivier@gmail.com>
2016-10-14 17:14:34 +0200

    tidying folder & files

---
+++ front/routeApp/js/app.js
@@ -0,0 +1,1 @@
+var angularComptaApp = angular.module('ComptApp', []);
--- front/routeApp/js/controllers.js
+++ front/routeApp/js/controllers.js
@@ -1,4 +1,13 @@
-var angularComptaApp = angular.module('ComptApp', []);
+var angularComptApp = angular.module('ComptApp', ['ngRoute']);
+
+angularComptApp.config(function($routeProvider){
+    $routeProvider
+        .when('/home', {templateUrl: 'parties/home.html'})
+        .when('/compte', {templateUrl: 'parties/compte.html'})
+        .when('/contact', {templateUrl: 'parties/contact.html'})
+        .otherwise({redirecTo : '/'});
+});
+
 
 angularComptaApp.controller('PostCtrl', function ($scope) {
     $scope.credits = [{
